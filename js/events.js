2234b55cd46d0fe5b4671fcca8ecf7b811c6a37c
Gabrielle Bellini <gbellini90@gmail.com>
2018-09-24 17:26:25 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,12 +1,24 @@
 function getIt()
 {$('p').on('click',function
 ()
-{
+  {
   alert('Hey!')
-  
-})
+  }
+)
+}
+
+function frameIt(){
+ $().on('load',function(){
+   
+   
+ }) 
   
 }
+
+
+
+
+
 
 $(document).ready(function(){
 
